8bf543a83dcd94992cec09de67e148fb584bfebc
Pit_2002 <83857592+PitZhu-2002@users.noreply.github.com>
2024-12-10 12:55:38 +0800

    Add files via upload

---
+++ heterogeneous_analysis/wtl_analysis/win_tie_loss.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+
+data = pd.read_excel('.\data\heter_new_mean.xls').set_index('Unnamed: 0')
+size = len(data.columns)
+idx = data.index
+df = pd.DataFrame()
+df2 = pd.DataFrame()
+for a in range(size):
+    way = data.columns[a]
+    win = 0
+    tie = 0
+    los = 0
+    count = 0
+    wtl_lst = []
+    for name in idx:
+        count = count + 1
+        if data.loc[name,'DES-SV'] == data.loc[name,way]:
+            wtl_lst.append('T')
+            tie = tie + 1
+        elif data.loc[name,'DES-SV'] > data.loc[name,way]:
+            wtl_lst.append('W')
+            win = win + 1
+        else:
+            wtl_lst.append('L')
+            los = los + 1
+    wtl_lst = np.array(wtl_lst)
+    df2.loc[:, data.columns[a]] = wtl_lst
+    result = [win,tie,los]
+    print(result)
+    three = ['win','tie','loss']
+    for n in range(len(three)):
+        df.loc[way,three[n]] = result[n]
+print(df.to_excel('heter_new_wtl.xls'))
+print(df2.to_excel('wtl_form_heter.xls'))
+
+
